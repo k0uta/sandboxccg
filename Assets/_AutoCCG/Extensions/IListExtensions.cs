fb5359ff8f435603f1cffae1b3ba2634233faa34
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2019-03-27 17:10:08 -0300

    Add WeightedShuffle to list extensions

--- Assets/_AutoCCG/Extensions/IListExtensions.cs
+++ Assets/_AutoCCG/Extensions/IListExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class IListExtensions
 {
@@ -16,5 +18,59 @@ public static class IListExtensions
             ts[i] = ts[r];
             ts[r] = tmp;
         }
+    }
+
+    public static void WeightedShuffle<T>(this IList<T> targetList, Func<T, float> weightFunction)
+    {
+        var weights = new List<float>();
+        List<int> invalidIds = new List<int>();
+
+        for (int i = 0; i < targetList.Count; i++)
+        {
+            float weight = weightFunction(targetList[i]);
+            if (weight > float.Epsilon)
+            {
+                weights.Add(weight);
+            } else
+            {
+                invalidIds.Add(i);
+            }
+        }
+
+        for (int i = 0; i < invalidIds.Count; i++)
+        {
+            var invalidId = invalidIds[i];
+            var listObject = targetList[invalidId];
+            targetList.RemoveAt(invalidId);
+            targetList.Add(listObject);
+        }
+
+        var newOrder = new List<int>();
+        for (int i = 0; i < weights.Count; i++)
+        {
+            var r = WeightedChoice(weights);
+
+            var tmp = targetList[i];
+            targetList[i] = targetList[r];
+            targetList[r] = tmp;
+
+            weights[r] = weights[i];
+            weights[i] = 0;
+        }
+    }
+
+    static int WeightedChoice(List<float> weights)
+    {
+        var random = UnityEngine.Random.value * weights.Sum();
+        for (int i = 0; i < weights.Count; i++)
+        {
+            random -= weights[i];
+            if (random < 0)
+            {
+                return i;
+            }
+        }
+
+        throw new KeyNotFoundException();
     }
 }
