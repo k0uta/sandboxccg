2cbe1c03cdcac9e49036e1f7454dabbb43a6eb6c
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2019-04-26 09:28:09 -0300

    Add AddHealthModifierToArea effect

---
+++ Assets/_AutoCCG/Cards/Effects/Scripts/AddHealthModifierToArea.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoCCG
+{
+    public class AddHealthModifierToArea : CardEffectModel
+    {
+        public int modifier;
+
+        public bool permanent;
+
+        public Area area;
+
+        public TargetPlayer targetPlayer;
+
+        public override List<ActionStepModel> CreateSteps(BattlegroundsCardModel battlegroundsCard)
+        {
+            var effectSteps = new List<ActionStepModel>();
+
+            var areaCards = battlegroundsCard.GetArea(area, targetPlayer);
+
+            foreach (var card in areaCards)
+            {
+                void ReverseStep() => card.cardModel.life -= modifier;
+                var modifierStep = new ActionStepModel(() => card.cardModel.life += modifier, !permanent ? (Action) ReverseStep : null);
+                effectSteps.Add(modifierStep);
+            }
+
+            return effectSteps;
+        }
+    }
+}
